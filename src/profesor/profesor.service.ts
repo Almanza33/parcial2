1a68fbe0fb96ed229112e4e6222baca49eaf421e
Almanza33 <d.almanza@uniandes.edu.co>
2025-05-15 10:40:22 -0500

    faltaba un servicio

--- src/profesor/profesor.service.ts
+++ src/profesor/profesor.service.ts
@@ -3,12 +3,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { ProfesorEntity } from './profesor.entity';
+import { EvaluacionEntity } from 'src/evaluacion/evaluacion.entity';
 
 @Injectable()
 export class ProfesorService {
   constructor(
     @InjectRepository(ProfesorEntity)
     private readonly profesorRepository: Repository<ProfesorEntity>,
+
+    @InjectRepository(EvaluacionEntity)
+    private readonly evaluacionRepository: Repository<EvaluacionEntity>,
   ) {}
 
   async crearProfesor(profesor: ProfesorEntity): Promise<ProfesorEntity> {
@@ -16,5 +20,27 @@ export class ProfesorService {
       throw new Error('La extensión debe tener 5 dis');
     }
     return await this.profesorRepository.save(profesor);
+  }
+  async asignarEvaluador(id: number, evaluacionID: number): Promise<void> {
+    const profesor = await this.profesorRepository.findOne({
+      where: { id },
+      relations: ['evaluaciones'],
+    });
+    if (!profesor) {
+      throw new Error('Profesor no encontrado');
+    }
+    const evaluacion = await this.evaluacionRepository.findOne({
+      where: { id: evaluacionID },
+      relations: ['profesor'],
+    });
+    if (!evaluacion) {
+      throw new Error('Evaluación no encontrada');
+    }
+
+    if (profesor.evaluaciones.length >= 3) {
+      throw new Error('El profe tiene muchas evals ya');
+    }
+    profesor.evaluaciones.push(evaluacion);
+    await this.profesorRepository.save(profesor);
   }
 }
